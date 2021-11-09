3dfbdaf74921e58a8e3d51a30e1cb1d16b678122
brightlightkim <taeyangk95@naver.com>
2021-11-09 19:23:50 +0900

    add basic data

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="app">
+    <div id="menu">
+      <div id="brand">
+        <router-link to="/">
+          <img src="/images/logo.png" />
+        </router-link>
+      </div>
+      <div id="side">
+        <router-link to="/browse">
+          <div class="menu-item browse">
+            <img src="/images/globe.png" />
+            <p>Browse</p>
+          </div>
+        </router-link>
+        <router-link to="/cart">
+          <div class="menu-item" v-bind:class="cart">
+            <img src="/images/love.png" />
+            <p>{{ numberOfItems }} items</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
+    <router-view />
+    <div id="footer">
+      <a href="https://github.com/BYUCS260/grocery-store-brightlightkim">GitHub Link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    numberOfItems() {
+      var cartLength = this.$root.$data.cart.length;
+      return cartLength;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 50px 100px;
+}
+
+#menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 5px;
+  grid-template-areas: "none brand side";
+  margin-bottom: 50px;
+}
+
+#menu a {
+  color: #b84901;
+}
+
+#brand {
+  grid-area: brand;
+  display: flex;
+  justify-content: center;
+}
+
+#brand img {
+  height: 200px;
+}
+
+#side {
+  grid-area: side;
+  display: flex;
+  justify-content: flex-end;
+}
+
+#side img {
+  width: 50px;
+}
+
+.menu-item {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-item p {
+  margin: 0px;
+}
+
+.browse {
+  margin-right: 50px;
+}
+
+#footer {
+  position: fixed;
+  bottom: 0;
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  color: white;
+}
+
+#footer a {
+  color: white;
+}
+</style>
